4d23f4e85cdfba9262c322325df8089cda172951
ViniGSouza <vinizintwitchtv@gmail.com>
2024-01-09 18:06:33 -0300

    first commit

---
+++ src/routes/admin.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { Request, Response } from "express";
+import * as auth from "../controllers/auth";
+import * as events from "../controllers/events";
+import * as groups from "../controllers/groups";
+
+const router = Router();
+
+router.post('/login', auth.login); 
+
+router.get('/ping', auth.validate, (req: Request, res: Response) => {
+  res.json({ pong: true, admin: true })
+})
+
+router.get('/events', auth.validate, events.getAll);
+router.get('/events/:id', auth.validate, events.getEvent);
+router.post('/events', auth.validate, events.addEvent);
+router.put('/events/:id', auth.validate, events.updateEvent);
+router.delete('/events/:id', auth.validate, events.deleteEvent);
+
+router.get('/events/:id_event/groups', auth.validate, groups.getAll);
+
+export default router;
